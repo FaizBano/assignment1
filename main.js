48f05f335c1abeb2aa7cc5314e62a5d8f1ac1dee
FaizBano <159723889+FaizBano@users.noreply.github.com>
2024-03-05 06:06:41 +0500

    Add files via upload
    this is my first assignment

---
+++ main.js
@@ -0,0 +1,5 @@
+// Assignmen # 1
+// storing the person name in a veriable
+var personname = "Nadeem";
+// printing the message to the personname
+console.log("hello ".concat(personname, "   would you please learn some python  with me"));
---
+++ main.ts
@@ -0,0 +1,9 @@
+// Assignmen # 1
+
+// storing the person name in a veriable
+
+let personname: string="Nadeem"
+
+// printing the message to the personname
+
+console.log(`hello ${personname}   would you please learn some python  with me`)
